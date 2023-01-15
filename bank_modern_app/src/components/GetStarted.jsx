6762c3fb62117bea03c448f2f0c565e6009ac716
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-01-15 10:04:37 +0700

    membuat get started (aduuh tibabaradug)

--- bank_modern_app/src/components/GetStarted.jsx
+++ bank_modern_app/src/components/GetStarted.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import styles from "../style"
+import { arrowUp } from "../assets"
 
-const GetStarted = () => {
-  return (
-    <div>GetStarted</div>
-  )
-}
+const GetStarted = () => (
+    <div className={`${styles.flexCenter} w-[140px] aspect-square rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+      <div className={`${styles.flexCenter} w-full aspect-square bg-primary rounded-full`}>
+        <div className={`${styles.flexStart} flex-col`}>
+          <p className="font-poppins font-medium text-[18px] leading-[23px]">
+            <span className='text-gradient'>Get</span>
+            <img src={arrowUp} alt="arrow" className="inline-block m-1 w-[23px] aspect-square object-contain" />
+          </p>
+          <p className="font-poppins font-medium text-[18px] leading-[23px]">
+            <span className='text-gradient'>Started</span>
+          </p>
+        </div>
+      </div>
+    </div>
+)
 
 export default GetStarted
