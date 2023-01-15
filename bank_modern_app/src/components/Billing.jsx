481150fc875b00fe2ee9d78699d45122a842071c
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-01-15 13:59:24 +0700

    billing section: image

--- bank_modern_app/src/components/Billing.jsx
+++ bank_modern_app/src/components/Billing.jsx
@@ -3,7 +3,15 @@ import styles, { layout } from "../style";
 
 const Billing = () => {
   return (
-    <div>Billing</div>
+    <section id="product" className={`${layout.sectionReverse}`}>
+      <div className={`${layout.sectionImgReverse}`}>
+        <img src={bill} alt="billing" className="w-full h-full relative z-[5]"/>
+
+        <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+        <div className="absolute z-[0] -left-1/2 top-0 w-[50%] h-[50%] rounded-full pink__gradient" />
+      </div>
+
+    </section>
   )
 }
 
