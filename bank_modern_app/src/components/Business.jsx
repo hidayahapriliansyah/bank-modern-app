9761f3128f26c69a6e5131ed86fc63268dab85bc
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-01-15 13:16:31 +0700

    bagian kanan business section

--- bank_modern_app/src/components/Business.jsx
+++ bank_modern_app/src/components/Business.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { features } from "../constants";
+import styles, { layout } from "../style";
+import Button from './Button';
+
+const FeatureCard = ({icon, title, content, index}) => (
+  <div className={`flex flex-row p-6 rounded-[20px]
+  ${index === features.length - 1 ? 'mb-6' : 'mb-0'} feature-card`}>
+
+  </div>
+);
 
 const Business = () => {
   return (
-    <div>Business</div>
+    <section id="features" className={`${layout.section}`}>
+      <div>
+        <h2 className={`${styles.heading2}`}>You do the business, <br className="sm:block hidden" /> we'll handle the money</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>With the right credit card, you can improve your financial life by building credit, earning rewards and saving money. But with hundreds of credit cards on the market.</p>
+        <Button styles="mt-10"/>
+      </div>
+
+      <div className={`${layout.sectionImg} flex-col`}>
+        {features.map((feature, index) => (
+          <FeatureCard key={feature.id} {...feature} index={index}/>
+        ))}
+      </div>
+    </section>
   )
 }
 
