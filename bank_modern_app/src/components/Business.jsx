fca6dc4c4ce2b01475f50a3e8e753dc91a83938d
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-01-15 13:30:20 +0700

    create feature card by mapping

--- bank_modern_app/src/components/Business.jsx
+++ bank_modern_app/src/components/Business.jsx
@@ -5,7 +5,14 @@ import Button from './Button';
 const FeatureCard = ({icon, title, content, index}) => (
   <div className={`flex flex-row p-6 rounded-[20px]
   ${index === features.length - 1 ? 'mb-6' : 'mb-0'} feature-card`}>
+    <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
+      <img src={icon} alt="icon" className="w-1/2 h-1/2 object-contain"/>
+    </div>
 
+    <div className="flex flex-1 flex-col ml-3">
+      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px]">{title}</h4>
+      <p className="font-poppins font-normal text-white text-[16px] leading-[24px]">{content}</p>
+    </div>
   </div>
 );
 
