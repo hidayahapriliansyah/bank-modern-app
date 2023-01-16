d2aef15a5cc1849da052f7b5ccb2e4d989d20b6c
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-01-16 07:33:32 +0700

    add some hover effect to client section

--- bank_modern_app/src/components/Clients.jsx
+++ bank_modern_app/src/components/Clients.jsx
@@ -3,9 +3,9 @@ import styles from "../style"
 
 const Clients = () => (
   <section className={`${styles.flexCenter} my-4`}>
-    <div className={`${styles.flexCenter} flew-wrap w-full`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
       {clients.map((client) => (
-        <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}>
+        <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] mb-10 md:my-0`}>
           <img src={client.logo} alt="client" className="sm:w-[192px] w-[100px] object-contain hover:brightness-200 hover:contrast-200" />
         </div>
       ))}
